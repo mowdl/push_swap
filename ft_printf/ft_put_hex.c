6a32d110e256055fa6fc4ff27c56edc9ad612644
mel-meka <mel-meka@student.1337.ma>
2024-03-27 01:47:07 +0000

    for linux cc

--- ft_printf/ft_put_hex.c
+++ ft_printf/ft_put_hex.c
@@ -6,7 +6,7 @@
 /*   By: mel-meka <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/26 14:59:15 by mel-meka          #+#    #+#             */
-/*   Updated: 2023/11/26 16:14:28 by mel-meka         ###   ########.fr       */
+/*   Updated: 2024/02/28 03:32:28 by mel-meka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,7 +49,8 @@ int	ft_put_hex(unsigned long n, int upper, int pointer)
 	else
 		str = "0123456789abcdef";
 	if (pointer)
-		write(1, "0x", 2);
+		if (write(1, "0x", 2) == -1)
+			return (-1);
 	hex(n, str);
 	return (digits_hex(n) + (pointer != 0) * 2);
 }
